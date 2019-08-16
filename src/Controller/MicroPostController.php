d99ce9053e1869d8dbbf301af4219e8c0c8b4e9b
Марков Андрей Игоревич <anrey-markov@mail.ru>
2019-08-16 16:56:28 +0300

    basics of doctrine repository

---
+++ src/Controller/MicroPostController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Repository\MicroPostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/micro-post")
+ *
+ * @property MicroPostRepository $repository
+ */
+class MicroPostController extends AbstractController
+{
+    /**
+     * @var MicroPostRepository
+     */
+    private $repository;
+
+    public function __construct(MicroPostRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route("/", name="micro_post_index")
+     */
+    public function index()
+    {
+        return $this->render('micro-post', [
+            'posts' => $this->repository->findAll()
+        ]);
+    }
+}
