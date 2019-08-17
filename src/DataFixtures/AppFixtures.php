5f043001e586c5569206b43b817f7f6187defdec
Марков Андрей Игоревич <anrey-markov@mail.ru>
2019-08-17 23:56:35 +0300

    user entity fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,12 +3,37 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+/**
+ * Class AppFixtures
+ * @package App\DataFixtures
+ *
+ * @property UserPasswordEncoderInterface $encoder
+ */
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
+    {
+        //$this->loadMicroPosts($manager);
+
+        $this->loadUsers($manager);
+    }
+
+    private function loadMicroPosts(ObjectManager $manager)
     {
         for ($i = 0; $i < 2; $i++) {
             /**
@@ -22,6 +47,23 @@ class AppFixtures extends Fixture
             $manager->persist($microPost);
         }
 
+        $manager->flush();
+    }
+
+    private function loadUsers(ObjectManager $manager)
+    {
+        /**
+         * @var User $user
+         */
+        $user = new User();
+        $user->setUsername('foo_bar');
+        $user->setFullName('Foo Bar');
+        $user->setEmail('foo_bar@mail.com');
+        $user->setPassword(
+            $this->encoder->encodePassword($user, 'secret')
+        );
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -143,4 +143,52 @@ class User implements UserInterface, \Serializable
     {
         list($this->id, $this->username, $this->email, $this->password) = unserialize($serialized);
     }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+
+    /**
+     * @param mixed $fullName
+     */
+    public function setFullName($fullName): void
+    {
+        $this->fullName = $fullName;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username): void
+    {
+        $this->username = $username;
+    }
 }
